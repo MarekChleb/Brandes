e0fcdcda5395ead4ed7712f6a735c189fa179429
Marek <mw332637@students.mimuw.edu.pl>
2017-01-09 21:38:54 +0100

    pre-beta2

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ void read_input(string input) {
             add_to_beg(a, b);
         file.close();
     }
-    else cout << "File error" << endl;
+    else cout << "Input error" << endl;
     n = beg_map.size();
 }
 
@@ -109,54 +109,21 @@ void do_brandes_instances() {
     }
 }
 
+void print_output_into_file(string output_file) {
+    ofstream file(output_file);
+    if (file.is_open()) {
+        for(int i = 0; i < n; i++) {
+            if(neighbour[i].size() > 0)
+                file << names[i] << " " << BC[i] << endl;
+        }
+        file.close();
+    }
+    else cout << "Output error" << endl;
+}
+
 int main(int argc, char *argv[]) {
 
-    /**
-     * TODO reserve:
-     * BC 0
-     * P list()
-     * sigma 0
-     * d -1
-     * delta 0
-     */
-    /*
-    for (int v: V) {
-        BC[v] = 0;
-    } */
-    BC.reserve(n);
-    for (int s = 0; s < n; s++) { //TODO parallel
-        vector<int> S = {}; //stack
-        sigma[s] = 1;
-        d[s] = 0;
-        vector<int> Q; //FIFO, queue, reverse kolejność, ew TODO change on
-        // queue
-        Q.insert(Q.begin(), s);
-        while (!Q.empty()) {
-            int v = Q.back();
-            Q.pop_back();
-            S.push_back(s);
-            for (int w: neighbour[v]) {
-                if (d[w] < 0) {
-                    Q.insert(Q.begin(), w);
-                    d[w] = d[v] + 1;
-                }
-                if (d[w] == d[v] + 1) {
-                    sigma[w] += sigma[v];
-                    P[w].push_back(v);
-                }
-            }
-        }
-        while (!S.empty()) {
-            int w = S.back();
-            S.pop_back();
-            for (int v: P[w]) {
-                delta[v] += (sigma[v] / sigma[w]) * (1 + delta[w]);
-            }
-            if (w != s) {
-                BC[w] += delta[w];
-            }
-        }
-    }
+
 
     return 0;
 }
